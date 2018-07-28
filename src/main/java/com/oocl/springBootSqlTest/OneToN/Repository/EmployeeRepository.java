b73222fba3b07e365472d020778c2105cc2fab1b
GUOAB <AbbyGuo@oocl.com>
2018-07-28 17:45:34 +0800

    add oneToOne repositoryTest

--- src/main/java/com/oocl/springBootSqlTest/OneToN/Repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springBootSqlTest/OneToN/Repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.oocl.springBootSqlTest.OneToN.Repository;
 
 import com.oocl.springBootSqlTest.OneToN.Enity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,5 +13,7 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     @Query("select t from Employee t where t.gender= ?1")
     List<Employee> findByGender(String status);
+
+    Page<Employee> findAll(Pageable pageable);
 
 }
--- src/test/java/com/oocl/springBootSqlTest/OneToN/Repository/EmployeeRepositoryTest.java
+++ src/test/java/com/oocl/springBootSqlTest/OneToN/Repository/EmployeeRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -33,5 +35,17 @@ public class EmployeeRepositoryTest {
         List<Employee> employees = employeeRepository.findByGender("male");
         //then
         assertThat(employees.size(),is(1));
+    }
+
+    @Test
+    public void findByPageTest(){
+        //given
+        testEntityManager.persist(new Employee("c1","male"));
+        testEntityManager.persist(new Employee("c2","female"));
+        //when
+        Page<Employee> companies = employeeRepository.findAll(PageRequest.of(1,1));
+        //then
+        assertThat(companies.getContent().size(),is(1));
+        assertThat(companies.getContent().get(0).getEmployeeName(),is("c2"));
     }
 }
